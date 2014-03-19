16a11077a510d86a97db1cc8b7983b9032280fe5
Robert Kuykendall <robert@rkuykendall.com>
2014-03-18 21:52:05 -0400

    Improved structure.

--- jit.py
+++ jit.py
@@ -1,28 +1,54 @@
 #!/usr/bin/python
 from optparse import OptionParser
 import glob
+import ply.yacc as yacc
+import ply.lex as lex
 
-class Program:
-	code = ""
-	
-	def __init__(self, path):
+# Good references:
+# https://github.com/xerox91/Simple-Compiler/blob/master/compiler.py
+# https://github.com/dabeaz/ply/blob/master/example/GardenSnake/GardenSnake.py
+
+
+class Lexing():
+	keywords = {}
+	tokens = {}
+	# lex.lex() ?
+
+class Parsing():
+	tokens = Lexing.tokens
+	# yacc.parse(s) ?
+
+class Compiler():
+	def __init__(self):
+		self.lex = Lexing()
+		self.parse = Parsing()
+		
+#	def init(self):
+#		self.lex.build()
+
+	def compile(self, path, verbose=False):
+		# Print file to screen
+		self.jit_code = open(path, 'r')
 		print path+":"
-		code = open(path, 'r')
-		for line in code:
+		for line in self.jit_code:
 			print "\t"+line
 		print "\n"
 
 def main():
 	parser = OptionParser()
+	jit_compiler = Compiler()
+#	jit_compiler.init()
+	
+	
 	parser.add_option("-f", "--file", dest="filename", help="JIT program filename", type="string")
 	(options, args) = parser.parse_args() 
 
 	if (options.filename):
-		prog = Program(options.filename)
+		jit_compiler.compile(options.filename, True)
 	else:
 		# Compile everything in this folder
 		for fname in glob.glob('*.jit'):
-			prog = Program(fname)
+			jit_compiler.compile(fname, True)
 
 if __name__ == '__main__':
     main()
