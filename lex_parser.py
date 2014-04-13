5af1d572bc09fd2992c59996f29dbee0adafa84c
kcecireyes <kcecireyes@gmail.com>
2014-04-12 20:48:44 -0400

    added more grammar rules, can successfully parse the 2nd program

--- lex_parser.py
+++ lex_parser.py
@@ -15,6 +15,7 @@ reserved = {
 	'search': 'SEARCH',
 	'save': 'SAVE',
 	'push': 'PUSH',
+	'get' : 'GET',
 	'pull': 'PULL',
 	'createNode': 'CREATENODE'
 }
@@ -35,7 +36,8 @@ tokens = [
 literals = ['+','-','*','/']
 
 # Regular expression rules for simple tokens
-t_EQUALS   = r'='
+# == 
+t_EQUALS = r'='
 t_COMMA  = r','
 t_LPAREN  = r'\('
 t_RPAREN  = r'\)'
@@ -55,9 +57,6 @@ def t_ID(t):
 	t.type = reserved.get(t.value,'ID') # Check for reserved words
 	return t
 
-# ((true|false|'.*'|[a-zA-Z_][a-zA-Z_0-9\_]*|[a-zA-Z_][a-zA-Z_0-9\_]*.[a-z]),)
-# DAMN THIS
-# \[( ( (true|false)* | ('.*')* | ([a-zA-Z_][a-zA-Z_0-9\_]*|[a-zA-Z_][a-zA-Z_0-9\_]*.[a-z])*)* ,)*\]
 def t_LIST_s(t):
 	r"\[(((true|false)* |('.*')*|([a-zA-Z_][a-zA-Z_0-9\_]*|[a-zA-Z_][a-zA-Z_0-9\_]*.[a-z])*)*,)*\]"
 	return t
@@ -75,9 +74,6 @@ def t_newline(t):
 	r'\n+'
 	t.lexer.lineno += len(t.value)
 
-#def t_WHITESPACE(t):
-#	r'\s'
-#	pass
 
 # Error handling rule
 def t_error(t):
@@ -121,18 +117,89 @@ type : STRING
 	  NODE
 	  LIST
 	  GRAPH
+
+S' -> statement
+statement -> variable_decl | function_call
+variable_decl -> type ID EQUALS expression
+				| ID EQUALS expression
+				| type ID
+function_call -> fun LPAREN parameters RPAREN
+fun -> SAY
+	| LISTEN
+	| IMPORT
+	| SAVE
+	| GET
+	| PUSH
+	| PULL
+	| SEARCH
+parameters -> empty
+			| parameter COMMA parameters
+			| parameter
+parameter -> ID
+			| STRING_s
+			| ID EQUALS expression
+type -> STRING
+		| BOOLEAN
+		| INT
+		| NODE
+		| LIST
+		| GRAPH
+expression -> arithmetic_expr
+			| function_call
+			| STRING_s
+			| BOOLEAN_s
+arithmetic_expr -> arithmetic_expr + term
+				 | arithmetic_expr - term
+				 | term
+term -> term * factor
+		| term / factor
+		| factor
+factor -> LPAREN arithmetic_expr RPAREN
+		| ID
+		| NUM
+empty -> <empty>
+
+
+
+
+
+
 '''
 
 # Parser for parsing variable declarations and initializations only
 
 def p_statement(p):
-	'statement : variable_decl'
-	
+	'''statement : variable_decl
+				 | function_call'''
+
 def p_variable_decl(p):
 	'''variable_decl : type ID EQUALS expression
 					 | ID EQUALS expression
 					 | type ID'''
-	
+
+def p_function_call(p):
+	'''function_call : fun LPAREN parameters RPAREN'''
+
+def p_fun(p):
+	'''fun : SAY
+			| LISTEN
+			| IMPORT
+			| SAVE
+			| GET
+			| PUSH
+			| PULL
+			| SEARCH'''
+
+def p_parameters(p):
+	'''parameters : empty
+				 | parameter COMMA parameters
+				 | parameter'''
+
+def p_parameter(p):
+	'''parameter : ID
+			    | STRING_s
+			    | ID EQUALS expression'''
+
 def p_type(p):
 	'''type : STRING
 			| BOOLEAN
@@ -142,7 +209,10 @@ def p_type(p):
 			| GRAPH'''
 
 def p_expression(p):
-	'expression : arithmetic_expr'
+	'''expression : arithmetic_expr
+				  | function_call
+				  | STRING_s
+				  | BOOLEAN_s'''
 
 def p_arithmetic_expr(p):
 	'''arithmetic_expr : arithmetic_expr '+' term
@@ -159,14 +229,17 @@ def p_factor(p):
 			  | ID
 			  | NUM'''
 
+def p_empty(p):
+    'empty :'
+    pass
+
 def p_error(p):
 	print("Syntax error at '%s'" % p.value)
-
 
 import ply.yacc as yacc
 yacc.yacc()
 
-file1 = open("program3.txt", "r") #Note: Input file must end with a new line character
+file1 = open("program3.txt", "r") # Note: Input file must end with a new line character
 data = file1.read()
 file1.close()
 
@@ -182,4 +255,4 @@ while 1:
 		break
 	yacc.parse(s)
 
-print 'End of lexing and parsing: If no errors were displayed give Prashant (and not Cecilia) a chocolate!\n...\n...\n...\nOh ya and it also means your source program is correct.'
+print 'success.'
