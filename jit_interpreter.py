dc8dac47cf4850616ba7097fe6d63248aaf3a2a1
Robert Kuykendall <robert@rkuykendall.com>
2014-04-16 20:35:07 -0400

    Removed JSON stuff since there is JSON function in Python.

--- jit_interpreter.py
+++ jit_interpreter.py
@@ -2,18 +2,22 @@ from jit_parser import *
 from pprint import pprint
 
 class Interpreter:
-    # def __init__(self):
 
     def execute_ast(self, ast_node):
         # # Something like this?
-        # ast_node.right = execute_ast(ast_node.right)
-        # ast_node.left = execute_ast(ast_node.left)
-        # if (ast_node.type = "Add"):
-            # ast_node.value = ast_node.left.value + ast_node.right.value
-            # ast_node.left.value = ast_node.left.value = None
-        print ast_node.child.value
-        pass
         
+        if hasattr(ast_node, 'right'):
+            ast_node.right = self.execute_ast(ast_node.right)
+            
+        if hasattr(ast_node, 'left'):
+            ast_node.left = self.execute_ast(ast_node.left)
+        
+        if (ast_node.type == "fun"):
+            if ast_node.subtype == "say":
+                print ast_node.child.value
+            elif ast_node.subtype == "listen":
+                raw_input("User input: ")
+            
     def execute_txt(self, code):
         parser = Parser()
         ast = parser.parser.parse(code)
