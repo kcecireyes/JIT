e24da41a92fffc161e66b61c2ea29060339bc48c
Bocheng Zhou <fromsquaresone@gmail.com>
2014-04-20 17:55:08 -0400

    add code_generator

---
+++ jit_code_generator.py
@@ -0,0 +1,12 @@
+class CodeGenerator:
+    def __init__(self):
+        pass
+
+    def generate_list(self, elements):
+        return '[%s]' % ', '.join(elements)
+
+    def generate_asssignment(self, lfh, rhs):
+        return '%s = %s' % (lfh, rhs)
+
+    def generate_print(self, string):
+        return 'print %s' % string
