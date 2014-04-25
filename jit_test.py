4da1faab2112c5d68b4e58efe6be331185bc7514
RaulMatias <raulmatias7@gmail.com>
2014-04-24 23:53:22 -0400

    some more refactoring

--- jit_test.py
+++ jit_test.py
@@ -4,29 +4,37 @@ import os
  
 class TextJIT(unittest.TestCase):
     def setUp(self):
-    	pass
+        self.jit = "./jit_cli.py"
+        self.flag = "-f"
+        self.prog_ext = ".txt"
+        self.ref_ext = ".py"
+        self.out_ext = ".gen.py"
+        self.program1 = "programs/program1-five_says"
+        self.program2 = ""
+        self.program3 = "programs/program3-assignments_and_math"
+        self.program4 = "programs/program4-simple_node"
+        self.program5 = ""
 
     def test_say_should_generate_python(self):
-        #run test program
-        prog = "./jit_cli.py -f programs/program1-five_says.txt"
-        ref = "programs/program1-five_says.py"
-        out = "programs/program1-five_says.gen.py"
+        prog = self.jit + " " + self.flag + " " + self.program1 + self.prog_ext
+        ref = self.program1 + self.ref_ext
+        out = self.program1 + self.out_ext
         self.assertTrue(self.compare(out, ref, prog))
 
     def test_listen_should_generate_python(self):
         pass
 
     def create_node_should_generate_python(self):
-        prog = "./jit_cli.py -f programs/program4-simple_node.txt"
-        ref = "programs/program4-simple_node.py"
-        out = "programs/program4-simple_node.gen.py"
+        prog = self.jit + " " + self.flag + " " + self.program2 + self.prog_ext
+        ref = self.program4 + self.ref_ext
+        out = self.program4 + self.out_ext
         self.assertTrue(self.compare(out, ref, prog))
         pass
 
     def test_binop_should_generate_python(self):
-        # prog = "./jit_cli.py -f programs/program3-assignments_and_math.txt"
-        # ref = "programs/program3-assignments_and_math.txt"
-        # out = "programs/program3-assignments_and_math.txt"
+        # prog = jit + " " + flag + " " + program3 + prog_ext
+        # ref = program3 + ref_ext
+        # out = program3 + out_ext
         # self.assertTrue(self.compare(out, ref, prog))
         pass
 
