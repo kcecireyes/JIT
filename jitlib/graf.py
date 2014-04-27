ca649c760753d578b4c66d696941b2b181cbfc16
RaulMatias <raulmatias7@gmail.com>
2014-04-27 18:05:40 -0400

    fixed bug in tests

--- jitlib/graf.py
+++ jitlib/graf.py
@@ -1,11 +1,11 @@
 class Graf():
     def __init__(self, json=None):
-    		self.node_list = []
-    		self.number_of_vert = 0
+    	self.node_list = []
+    	self.number_of_vert = 0
     
     def push(self, node_list):
-    		self.number_of_vert += node_list.length
-    		self.node_list.extend(node_list)
+        self.number_of_vert += node_list.length
+    	self.node_list.extend(node_list)
     
     def push_node(self,node):
         # We might not need this
