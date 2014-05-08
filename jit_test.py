ddadb36bff4be564eb98bac31e0ecf0b61b29fde
RaulMatias <raulmatias7@gmail.com>
2014-05-07 21:25:50 -0400

    moar tests

--- jit_test.py
+++ jit_test.py
@@ -17,10 +17,12 @@ class TextJIT(unittest.TestCase):
         self.program3 = "programs/program3-assignments_and_math"
         self.program4 = "programs/program4-simple_node"
         self.program5 = "programs/program5-simple_variable"
+        self.program6 = "programs/program6-blocks"
         self.program7 = "programs/program7-pushing_and_pulling"
+        self.program9 = "programs/program9-saving_and_stuff"
 
-    def test_should_assign_variables(self):
-        self.assertTrue(self.compare(self.program5))        
+    def test_comments_should_be_parsed_out(self):
+        self.assertTrue(self.compare(self.program0))
 
     def test_say_should_generate_python(self):
         self.assertTrue(self.compare(self.program1))
@@ -28,17 +30,24 @@ class TextJIT(unittest.TestCase):
     def test_listen_should_generate_python(self):
         self.assertTrue(self.compare(self.program2))
 
-    def test_simple_node_should_generate_python(self):
-        self.assertTrue(self.compare(self.program4))
-
     def test_binop_should_generate_python(self):
         self.assertTrue(self.compare(self.program3))
 
-    def test_comments_should_be_parsed_out(self):
-        self.assertTrue(self.compare(self.program0))
+    def test_simple_node_should_generate_python(self):
+        self.assertTrue(self.compare(self.program4))
+
+    def test_should_assign_variables(self):
+        self.assertTrue(self.compare(self.program5))  
+
+    def test_should_allow_blocks(self):
+        self.assertTrue(self.compare(self.program6))  
 
     def test_pushing_and_pulling(self):
         self.assertTrue(self.compare(self.program7))
+
+    def test_should_save_and_import(self):
+        self.assertTrue(self.compare(self.program9))  
+
 
     def compare(self, program):
         prog = self.jit + " " + self.flag + " " + program + self.prog_ext
