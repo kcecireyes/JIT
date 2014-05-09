f085df8aceaf38792bc7258409cf3412583ff7fc
Robert Kuykendall <robert@rkuykendall.com>
2014-05-08 20:51:58 -0400

    Remove collection. Rename graf collection.

--- jitlib/collection.py
+++ jitlib/collection.py
@@ -1,26 +1,49 @@
-class Collection():
-    # node_list
+class Graf():
+    def __init__(self, json=None):
+    	self.node_list = []
+    	self.number_of_vert = 0
     
-    def __init__(self, node_list=None):
-        if node_list:
-            self.node_list = node_list
-            self.len = len(node_list)
-        else:
-        		self.node_list = []
-        		self.len = 0
+    def push(self, node_list):
+        self.number_of_vert += node_list.length
+    	self.node_list.extend(node_list)
+    
+    def push_node(self,node):
+        # We might not need this
+        self.number_of_vert += 1
+        self.node_list.append(node)
 
-    def add_node(self, node):
-    		self.node_list.append(node)
-    		self.len += 1
+    def push_collection(self, collection):
+        # We might not need this
+        self.number_of_vert += collection.len
+        self.node_list.extend(collection.node_list)
 
-    def get_nodes(self):
-    		return self.node_list
+    def add_edge(self, node1, node2):
+        # needs work, what does it mean for 2 articles to be the same
+        if node1 not in self.node_list:
+            self.node_list.append(node1)
+        if node2 not in self.node_list:
+            self.node_list.append(node2)
+
+        node1.add_adjacent(node2)
+        node2.add_adjacent(node1)
 
     def contains(self, node):
-    		if node in self.node_list:
-    				return True
-    		else:
-    				return False
+        if node in self.node_list:
+            return True
+        else:
+            return False
 
-    def to_json(self):
-        pass
+    def get_node(self, node):
+        for i in self.node_list:
+            if i == node:
+                return node
+
+    def get_node_by_title(self, title):
+        for i in self.node_list:
+            if i.title == title:
+                return i
+
+    def print_graf(self):
+        for i in self.node_list:
+            for n in i.adjacencies:
+                print ("( %s , %s )" % (i.title, n.title))
