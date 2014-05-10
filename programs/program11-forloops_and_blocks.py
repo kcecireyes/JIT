8f6f9c26da337bf27d9585c755fa45a2759e224f
RaulMatias <raulmatias7@gmail.com>
2014-05-10 18:10:39 -0400

    more text fixing

--- programs/program11-forloops_and_blocks.py
+++ programs/program11-forloops_and_blocks.py
@@ -1,9 +1,15 @@
 a = [4, 2, 3,]
-for x in y:
 for b in a:
 	c = b + 1
 	print '\n'.join(["hello"])
 	d = 0
+
+y = [1,2,3,]
+for x in y:
+	pass
+
 words = ["Shut", "the", "f**k", "up", "!",]
 for word in words:
 	print '\n'.join([word])
+
+	
---
+++ programs/program16-operations.py
@@ -0,0 +1,14 @@
+a = 2 + 3 / 4
+a = not true or false and true
+c = not 2 + 3 and 4 <= 3 and 5 / 7 > 9 == 8 + true or false
+if a == not b:
+	a = 0
+	a = 4 + 6
+else:
+	b = 3
+	print '\n'.join([b])
+
+if a >= b and c <= 3 or not b == c:
+	pass
+else:
+	pass
