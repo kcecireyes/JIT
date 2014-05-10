5aa9a9e89eae28c0d4eb90d82405606f14de674e
RaulMatias <raulmatias7@gmail.com>
2014-05-10 18:45:45 -0400

    program fix

--- programs/program2-nodes.py
+++ programs/program2-nodes.py
@@ -3,8 +3,8 @@ myNode = Node()
 myNode.title = "The Never-Ending Controversy Over All-Girls Education"
 myNode.author = "CHRISTINE GROSS-LOH"
 print '\n'.join([myNode.author])
-myNode.author = Listen()
+myNode.author = raw_input()
 myNode.date = "March 21 2014"
 myNode.publisher = "The Atlantic"
-myNode.keywords = ["girls", "education", "debate",]
+myNode.set_keywords(["girls", "education", "debate",])
 myNode.body = "string_body"
--- programs/program6-blocks.py
+++ programs/program6-blocks.py
@@ -1,7 +1,10 @@
 print '\n'.join(["simple program to test blocks"])
 a = 5
 print '\n'.join([a])
+b = [1,2,3,]
 for a in b:
+	pass
+
 print '\n'.join([x])
 print '\n'.join(["this is in a"])
 print '\n'.join([a])
